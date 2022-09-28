28b6adfc6d4960fca5a7a76b6af1545ab68d5eeb
codernoyan <coder.noyan@gmail.com>
2022-09-28 23:37:09 +0600

    activity card created

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './Activity.css';
 
-const Activity = () => {
+const Activity = (props) => {
+    console.log(props.activity);
+    const { title, image, timeRequired } = props.activity;
     return (
         <div>
-            <h1>Hello from activity</h1>
+            <div className="block overflow-hidden border border-gray-100 rounded-lg shadow-sm h-[400px]">
+                <img alt="court" src={image} className="object-cover w-full h-56" />
+                <div className="p-6">
+                    <h5 className="text-sm font-bold">{title}</h5>
+                    <p className="mt-2 text-sm text-gray-500">Time Required: {timeRequired}</p>
+                </div>
+                {/* <button className="inline-block pb-1 mt-4 font-medium text-blue-600 border-b border-blue-500">
+                    Add to List
+                    <span aria-hidden="true">&rarr;</span>
+                </button> */}
+                <button className="relative inline-block px-8 py-3 overflow-hidden border border-indigo-600 group focus:outline-none focus:ring w-full bottom-0">
+                    <span className="absolute inset-x-0 bottom-0 h-[2px] transition-all bg-indigo-600 group-hover:h-full group-active:bg-indigo-500"></span>
+                    <span className="relative text-sm font-medium text-indigo-600 transition-colors group-hover:text-white">Add to List</span>
+                </button>
+            </div>
+
         </div>
     );
 };
--- src/components/Club/Club.js
+++ src/components/Club/Club.js
@@ -5,8 +5,8 @@ import './Club.css';
 
 const Club = () => {
     return (
-        <div className="grid grid-cols-5 gap-8">
-            <div className="club-activities col-span-4">
+        <div className="grid grid-cols-5 gap-10">
+            <div className="club-activities col-span-4 my-12 mx-20">
                 <Activities></Activities>
             </div>
             <div className="club-dashboard">
