8a076b6de97d81d99686c7b2d1434001a1112d55
codernoyan <coder.noyan@gmail.com>
2022-09-29 19:25:44 +0600

    changed background color on activity details

--- src/components/Break/Break.js
+++ src/components/Break/Break.js
@@ -5,11 +5,11 @@ const Break = ({ShowBreakSeconds}) => {
     return (
         <div>
             <div className="mb-8 flex justify-around items-center bg-slate-100 p-3 rounded-xl">
-                <button onClick={(e) => ShowBreakSeconds(e)} className="rounded-full bg-white p-2">10s</button>
-                <button onClick={(e) => ShowBreakSeconds(e)} className="rounded-full bg-white p-2">20s</button>
-                <button onClick={(e) => ShowBreakSeconds(e)} className="rounded-full bg-white p-2">30s</button>
-                <button onClick={(e) => ShowBreakSeconds(e)} className="rounded-full bg-white p-2">40s</button>
-                <button onClick={(e) => ShowBreakSeconds(e)} className="rounded-full bg-white p-2">50s</button>
+                <button onClick={(e) => ShowBreakSeconds(e)} className="rounded-full bg-white p-2 font-medium">10s</button>
+                <button onClick={(e) => ShowBreakSeconds(e)} className="rounded-full bg-white p-2 font-medium">20s</button>
+                <button onClick={(e) => ShowBreakSeconds(e)} className="rounded-full bg-white p-2 font-medium">30s</button>
+                <button onClick={(e) => ShowBreakSeconds(e)} className="rounded-full bg-white p-2 font-medium">40s</button>
+                <button onClick={(e) => ShowBreakSeconds(e)} className="rounded-full bg-white p-2 font-medium">50s</button>
             </div>
         </div>
     );
